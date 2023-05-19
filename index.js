77769597691ef8c561e3acf6377b254a87df685a
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2023-05-18 23:22:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalDate = (date) => date.toUTCString === "Invalid Date"
+
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) { 
+  let date = new Date(req.params.date)
+
+  if(isInvalDate(date)){
+    date = new Date(+req.params.date)
+  }
+
+  if(isInvalDate(date)){
+    res.json({error: "Invalid Date"})
+    return;
+  }
+
+  res.json({
+    unix: date.getTime()
+    utc: date.toUTCString()
+  });
 });
+
+app.get("/api/", (req, res) ==> {
+  res.json({
+    unix: new Date().getTime()
+    utc: new Date() toUTCString()
+  })
+})
 
 
 
